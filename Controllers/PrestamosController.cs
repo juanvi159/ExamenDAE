5e5e90d5db8a6e7b8c67c26ef665ae3350f5f1fe
juanvi159 <juan.obando.a@tecsup.edu.pe>
2021-11-30 12:15:05 -0500

    examen

---
+++ Controllers/PrestamosController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Examen4.Controllers
+{
+    public class PrestamosController : Controller
+    {
+        #region Contexto
+        private BibliotecaEntities _contexto;
+        public BibliotecaEntities Contexto
+        {
+            set { _contexto = value; }
+            get
+            {
+                if (_contexto == null)
+                    _contexto = new BibliotecaEntities();
+                return _contexto;
+            }
+        }
+        #endregion
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View(Contexto.Prestamos.ToList());
+        }
+
+        // GET: /Categories/Create
+        //mostrar formulario
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+    }
+}
